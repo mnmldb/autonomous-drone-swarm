c5377d11d5258f5800241803672578b64cb621bf
Daisuke Nakanishi <dsknkns@gmail.com>
2021-10-25 20:19:45 -0400

    change ql.py to return a Q value when making an action

--- QL/ql.py
+++ QL/ql.py
@@ -2,7 +2,7 @@
 # each agent has an individual q table
 
 class QTables():
-    def __init__(self, observation_space, action_space, eps=1, gamma=0.9, r=0.99, lr=0.1):
+    def __init__(self, observation_space, action_space, eps_start=1, eps_end=0.1, gamma=0.9, r=0.99, lr=0.1):
         self.num_agents = len(observation_space)
 
         self.observation_space = observation_space
@@ -14,7 +14,8 @@ class QTables():
         self.action_values = [0, 1, 2, 3] # corresponding to the column numbers in q table
         self.action_num = len(self.action_values) # 4
 
-        self.eps = eps  # intial epsilon
+        self.eps = eps_start  # current epsilon
+        self.eps_end = eps_end # epsilon lower bound
         self.r = r  # decrement rate of epsilon
         self.gamma = gamma  # discount rate
         self.lr = lr  # learning rate
@@ -33,27 +34,42 @@ class QTables():
         obs_power = [v * (self.observation_num ** i) for i, v in enumerate(obs_shift)] # apply exponentiation to each element
         return sum(obs_power) # return the sum (results are between 0 and 256)
     
-    def get_action(self, observations):
+    def get_action(self, observations, stuck_counts, max_stuck, e_greedy=True):
         action_n = []
         greedy_n = [] # True if the action is determined by Q Table
+        action_value_n = []
 
         # get action for each agent
         for agent_i in range(self.num_agents):
-            # epsilon greedy
-            if np.random.rand() < self.eps:
+            # convert the observation to a row number
+            obs_row = self.obs_to_row(observations[agent_i])
+            if stuck_counts[agent_i] >= max_stuck: # random action to avoid stuck
                 action_n.append(random.choice(self.action_values))
                 greedy_n.append(False)
-            else:
-                # convert the observation to a row number
+                action_value_n.append(self.q_tables[agent_i][obs_row][action_n[agent_i]])
+            elif e_greedy: # epsilon greedy for training
+                if np.random.rand() < self.eps:
+                    action_n.append(random.choice(self.action_values))
+                    greedy_n.append(False)
+                    action_value_n.append(self.q_tables[agent_i][obs_row][action_n[agent_i]])
+                else:
+                    action_n.append(np.argmax(self.q_tables[agent_i][obs_row]))
+                    greedy_n.append(True)
+                    action_value_n.append(self.q_tables[agent_i][obs_row][action_n[agent_i]])
+            else: # greedy for test
                 obs_row = self.obs_to_row(observations[agent_i])
                 action_n.append(np.argmax(self.q_tables[agent_i][obs_row]))
                 greedy_n.append(True)
+                action_value_n.append(self.q_tables[agent_i][obs_row][action_n[agent_i]])
+      
+                greedy_n.append(True)
         
+        return action_n, greedy_n, action_value_n
+    
+    def update_eps(self):
         # update the epsilon
-        if self.eps > 0.1: # lower bound
+        if self.eps > self.eps_end: # lower bound
             self.eps *= self.r
-        
-        return action_n, greedy_n
 
     def train(self, obs, obs_next, actions, rewards):
         for agent_i in range(self.num_agents):
