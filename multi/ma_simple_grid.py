8bdb12326219cbc2a129cb2d8acd029180331e96
Daisuke Nakanishi <dsknkns@gmail.com>
2021-10-25 20:13:23 -0400

    change ma_simple_grid.py to the sequential action setting

--- multi/ma_simple_grid.py
+++ multi/ma_simple_grid.py
@@ -1,5 +1,3 @@
-# FOV: 4 cells (up, bottom, right, left)
-
 class Grid(gym.Env):
     metadata = {'render.modes': ['console']}
     # action id
@@ -8,7 +6,7 @@ class Grid(gym.Env):
     YM = 2 # y minus
     YP = 3 # y plus
     
-    def __init__(self, x_size=5, y_size=5, n_agents=2, fov_x=3, fov_y=3, simple=False):
+    def __init__(self, x_size=5, y_size=5, n_agents=2, fov_x=3, fov_y=3):
         super(Grid, self).__init__()
         
         # size of 2D grid
@@ -17,6 +15,7 @@ class Grid(gym.Env):
 
         # number of agents
         self.n_agents = n_agents
+        self.idx_agents = list(range(n_agents)) # [0, 1, 2, ..., n_agents - 1]
         
         # initialize the mapping status
         self.init_grid()
@@ -29,18 +28,12 @@ class Grid(gym.Env):
         self.action_space = MultiAgentActionSpace([spaces.Discrete(n_actions) for _ in range(self.n_agents)])
         
         # define observation space (fielf of view)
-        self.simple_fov = simple
         self.fov_x = fov_x # number of cells around the agent
         self.fov_y = fov_y # number of cells around the agent
 
-        if self.simple_fov:
-            self.obs_low = -np.ones(4) * 2
-            self.obs_high = np.ones(4) # -2: out of the grid, -1: obstacle, 0: not visited, 1: visited
-            self.observation_space = MultiAgentObservationSpace([spaces.Box(self.obs_low, self.obs_high) for _ in range(self.n_agents)])
-        else:
-            self.obs_low = -np.ones(self.fov_x * self.fov_y) * 2
-            self.obs_high = np.ones(self.fov_x * self.fov_y) # -2: out of the grid, -1: obstacle, 0: not visited, 1: visited
-            self.observation_space = spaces.Box(low=self.obs_low, high=self.obs_high)# low [-2, -2, ..., -2], high [1, 1, ..., 1]
+        self.obs_low = -np.ones(4) * 2 # low -2: out of the grid
+        self.obs_high = np.ones(4) # high 1: visited
+        self.observation_space = MultiAgentObservationSpace([spaces.Box(self.obs_low, self.obs_high) for _ in range(self.n_agents)])
     
     def init_agent(self):
         # initialize the agent position
@@ -109,7 +102,7 @@ class Grid(gym.Env):
         # initialize the position of the agent
         self.init_agent()
         
-        # surrounded by obstacles
+        # check if the drones at initial positions are surrounded by obstacles
         while True:
             obs = self.get_agent_obs()
             obs_tf = []
@@ -124,73 +117,57 @@ class Grid(gym.Env):
 
         return self.get_agent_obs()
         
-    def step(self, action_n):
-        reward_n = []
-        done_n = []
-        info_n = []
+    def step(self, action, i): # i: index of the drone
+        # original position
+        org_x  = self.agent_pos[i][0]
+        org_y  = self.agent_pos[i][1]
 
-        for i in range(self.n_agents):
-            action = action_n[i]
-            # original position
-            org_x  = self.agent_pos[i][0]
-            org_y  = self.agent_pos[i][1]
-
-            # move the agent
-            if action == self.XM:
-                self.agent_pos[i][0] -= 1
-            elif action == self.XP:
-                self.agent_pos[i][0] += 1
-            elif action == self.YM:
-                self.agent_pos[i][1] -= 1
-            elif action == self.YP:
-                self.agent_pos[i][1] += 1
-            else:
-                raise ValueError("Received invalid action={} which is not part of the action space".format(action))
-            
-            # account for the boundaris of the grid (-2: out of the grid)
-            if self.agent_pos[i][0] > self.x_size - 1 or self.agent_pos[i][0] < 0 or self.agent_pos[i][1] > self.y_size - 1 or self.agent_pos[i][1] < 0:
+        # move the agent
+        if action == self.XM:
+            self.agent_pos[i][0] -= 1
+        elif action == self.XP:
+            self.agent_pos[i][0] += 1
+        elif action == self.YM:
+            self.agent_pos[i][1] -= 1
+        elif action == self.YP:
+            self.agent_pos[i][1] += 1
+        else:
+            raise ValueError("Received invalid action={} which is not part of the action space".format(action))
+        
+        # account for the boundaries of the grid (-2: out of the grid)
+        if self.agent_pos[i][0] > self.x_size - 1 or self.agent_pos[i][0] < 0 or self.agent_pos[i][1] > self.y_size - 1 or self.agent_pos[i][1] < 0:
+            self.agent_pos[i][0] = org_x
+            self.agent_pos[i][1] = org_y 
+            self.grid_counts[self.agent_pos[i][0], self.agent_pos[i][1]] += 1
+            reward = 0
+        else:
+            # previous status of the cell
+            prev_status = self.grid_status[self.agent_pos[i][0], self.agent_pos[i][1]]
+            if prev_status == -1: # the new position is on the obstacle
+                # go back to the original position
                 self.agent_pos[i][0] = org_x
-                self.agent_pos[i][1] = org_y 
+                self.agent_pos[i][1] = org_y
                 self.grid_counts[self.agent_pos[i][0], self.agent_pos[i][1]] += 1
                 reward = 0
-            else:
-                # previous status of the cell
-                prev_status = self.grid_status[self.agent_pos[i][0], self.agent_pos[i][1]]
-                if prev_status == -1: # the new position is on the obstacle
-                    # go back to the original position
-                    self.agent_pos[i][0] = org_x
-                    self.agent_pos[i][1] = org_y
-                    self.grid_counts[self.agent_pos[i][0], self.agent_pos[i][1]] += 1
-                    reward = 0
-                elif prev_status == 0:
-                    self.grid_counts[self.agent_pos[i][0], self.agent_pos[i][1]] += 1
-                    reward = 10
-                elif prev_status == 1:
-                    self.grid_counts[self.agent_pos[i][0], self.agent_pos[i][1]] += 1
-                    reward = 0
-
-            reward_n.append(reward)
-        
-            # update the stuck count
-            if org_x == self.agent_pos[i][0] and org_y == self.agent_pos[i][1]: # stuck
-                self.stuck_counts[i] += 1
-            else:
-                self.stuck_counts[i] = 0
-
-        # modification: update the cell status after defining the reward so that we can treat all drones equally
-        for i in range(self.n_agents):
-            if self.grid_status[self.agent_pos[i][0], self.agent_pos[i][1]] == 0: # previous status is 0
+            elif prev_status == 0:
+                self.grid_counts[self.agent_pos[i][0], self.agent_pos[i][1]] += 1
                 self.grid_status[self.agent_pos[i][0], self.agent_pos[i][1]] = 1
+                reward = 10
+            elif prev_status == 1:
+                self.grid_counts[self.agent_pos[i][0], self.agent_pos[i][1]] += 1
+                reward = 0
+
+        # update the stuck count
+        if org_x == self.agent_pos[i][0] and org_y == self.agent_pos[i][1]: # stuck
+            self.stuck_counts[i] += 1
+        else:
+            self.stuck_counts[i] = 0
 
         # are we map all cells?
         mapped_poi = (self.grid_status == 1).sum()
         done = bool(mapped_poi == self.n_poi)
-        done_n = [done] * self.n_agents
-
-        # optionally we can pass additional info
-        info = {}
         
-        return self.get_agent_obs(), reward_n, done_n, info
+        return self.get_agent_obs(), reward, done
 
     def close(self):
         pass
